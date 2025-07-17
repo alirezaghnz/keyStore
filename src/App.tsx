0ef0fcbadd1c79de318f0de057cfa4a642a63ca4
alirezaghnzd <changlee76301@gmail.com>
2025-07-18 00:49:38 +0330

    create some file for started the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import AppLayOut from "./ui/AppLayOut";
+import NotFound from "./ui/NotFound";
+import Home from "./ui/Home";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayOut />,
+    errorElement: <NotFound />,
+    children: [{ path: "/", element: <Home /> }],
+  },
+]);
+
 function App() {
-  return <div>Hello</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/ui/AppLayOut.tsx
+++ src/ui/AppLayOut.tsx
@@ -1,1 +1,17 @@
-export default function AppLayOut() {}
+import { Outlet } from "react-router";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function AppLayOut() {
+  return (
+    <div className="grid grid-rows-[auto_1fr_auto] h-screen overflow-scroll">
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
